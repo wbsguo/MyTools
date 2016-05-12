23d1160248aeda5cf1197162015c0f70e77f36e4
wangbs <563492052@qq.com>
2016-05-12 16:12:54 +0800

    修改

--- mytoolslibrary/src/main/java/com/example/mytoolslibrary/utils/ActivitysUtils.java
+++ mytoolslibrary/src/main/java/com/example/mytoolslibrary/utils/ActivitysUtils.java
@@ -1,12 +1,13 @@
 package com.example.mytoolslibrary.utils;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
 /**
  * Activity的管理器，用map集合可以指定finish某个activity
  * @author Administrator
@@ -29,11 +30,23 @@ public class ActivitysUtils {
 			return null;
 		}
 	}
+	public boolean isContainsKey(String tag){
+		if(acHashMap.containsKey(tag)){
+			return true;
+		}else {
+			return false;
+		}
+	}
 	public void addActivity(String tag, Activity activity) {
 		//重复的会自动覆盖以前的
 		acHashMap.put(tag, activity);
 	}
-	public void finshActivity(List<String> activitys){
+	public void removeActivity(String tag){
+		if(acHashMap.containsKey(tag)){
+			acHashMap.remove(tag);
+		}
+	}
+	public void finshActivitys(List<String> activitys){
 		if(activitys.size()>0){
 			for(String string:activitys){
 				if(acHashMap.containsKey(string)){
@@ -47,14 +60,17 @@ public class ActivitysUtils {
 			}
 		}
 	}
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
 	public void destory() {
 		Iterator iter = acHashMap.entrySet().iterator();
 		while (iter.hasNext()) {
 			Map.Entry entry = (Map.Entry) iter.next();
-			String key = (String) entry.getKey();
 			Activity activity = (Activity) entry.getValue();
-			activity.finish();
+			if(!activity.isDestroyed()){
+				activity.finish();
+			}
 		}
 		acHashMap.clear();
+		System.exit(0);
 	}
 }
